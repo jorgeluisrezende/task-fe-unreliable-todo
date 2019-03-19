f640cf6ef07f2d1373e631f565faf410a76887e0
Jakub Matuska <jkb.matuska@gmail.com>
2019-03-19 08:33:41 +0100

    Add sessionId header to CORS

--- src/api/cors.js
+++ src/api/cors.js
@@ -1,7 +1,7 @@
 const cors = (app) => {
 	app.use((req, res, next) => {
 		res.header('Access-Control-Allow-Origin', '*')
-		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, sessionId')
 		next()
 	})
 }
